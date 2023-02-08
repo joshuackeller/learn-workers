196ca4172115b92270fbe5c9dd1fe5cd170f34a3
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2023-02-07 21:30:58 -0700

    show millard

--- src/routes/Testing/index.ts
+++ src/routes/Testing/index.ts
@@ -2,7 +2,7 @@ import { IRequest } from "itty-router";
 import res from "../../utilities/CustomResponse";
 
 const get = async (request: IRequest) => {
-  return res({ testing: "Ok now I'm testing it with github actions" });
+  return res({ testing: "Millard Jensen is the hottest" });
 };
 
 export default async function (request: IRequest) {
