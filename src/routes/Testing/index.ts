324ea43b428b6f3c7b1aab4e6f2d654ad339c34a
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2023-02-07 20:24:36 -0700

    testing cicd

--- src/routes/Testing/index.ts
+++ src/routes/Testing/index.ts
@@ -2,7 +2,7 @@ import { IRequest } from "itty-router"
 import res from "../../utilities/CustomResponse"
 
 const get = async (request: IRequest) => {
-    return res({testing: "testing"})
+    return res({testing: "Ok now I'm testing it with github actions"})
 }
 
 export default async function(request: IRequest) {
